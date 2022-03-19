18bb71180cd62e03d19d33cbd3c319c8e646af73
Mihai-George Sturza <sturzamihai23@gmail.com>
2022-03-20 00:50:27 +0200

    Final touches

--- scripts/make-deploy.sh
+++ scripts/make-deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "Going to root directory of repository..."
 cd ~/real
+
+echo "Checking for updates on main branch..."
 git pull
+
+echo "Updating dependencies..."
 npm ci
+
+echo "Creating build files..."
 npm run build
+
+echo "Restarting PM2 processes..."
 pm2 delete all
 pm2 start infrastructure.config.js
