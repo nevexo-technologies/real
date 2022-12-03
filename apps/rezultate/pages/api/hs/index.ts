ba212dcf8015ec310f0534591927bac98fa7ad90
Mihai-George Sturza <contact@mihaisturza.ro>
2022-12-03 02:34:04 +0200

    Added filter on Hs API

--- apps/rezultate/pages/api/hs/index.ts
+++ apps/rezultate/pages/api/hs/index.ts
@@ -1,14 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '@real/database';
+import { Elev, Parinte, prisma, Profesor } from '@real/database';
 import rateLimit from 'helpers/rateLimit';
-import getHsMetrics from 'helpers/getHsMetrics';
+import getHsMetrics, { HsProcessed } from 'helpers/getHsMetrics';
+import groupHs from 'helpers/groupHs';
 
 const limiter = rateLimit({
     interval: 60 * 1000, // 60 seconds
     uniqueTokenPerInterval: 500, // Max 500 users per second
 })
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         await limiter.check(res, 500, 'API_LIMIT_TOKEN');
     }
@@ -16,78 +17,74 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         return res.status(429).json({ message: 'Rate limit exceeded' });
     }
 
-    const studentResults = await prisma.elev.findMany({
-        select: {
-            hs: true,
-        },
-    });
+    if (req.method == 'GET') {
+        const { filterBy } = req.query;
 
-    const teachersResults = await prisma.profesor.findMany({
-        select: {
-            hs: true,
-        },
-    });
+        const realFilter = filterBy === "real";
 
-    const parentResults = await prisma.parinte.findMany({
-        select: {
-            hs: true,
-        },
-    });
+        const studentResults = await prisma.elev.findMany({
+            ...(!realFilter && {
+                select: {
+                    hs: true,
+                }
+            })
+        });
 
-    const results = [studentResults, teachersResults, parentResults].flat();
+        const teachersResults = await prisma.profesor.findMany({
+            ...(!realFilter && {
+                select: {
+                    hs: true,
+                }
+            })
+        });
 
-    let highschools = [] as { name: string, records: number }[];
-    for (let i = 0; i < results.length; i++) {
-        if (highschools.length == 0) {
-            highschools.push({ name: results[i].hs, records: 1 });
-        } else {
-            let found = false;
-            for (let j = 0; j < highschools.length; j++) {
-                if (highschools[j].name == results[i].hs) {
-                    highschools[j].records++;
-                    found = true;
-                    break;
+        const parentResults = await prisma.parinte.findMany({
+            ...(!realFilter && {
+                select: {
+                    hs: true,
+                }
+            })
+        });
+
+        if (realFilter) {
+            const groupedStudents = groupHs(studentResults);
+            const groupedTeachers = groupHs(teachersResults);
+            const groupedParents = groupHs(parentResults);
+
+            const groupedResults = Object.keys(groupedStudents).reduce<({ hs: string } & HsProcessed)[]>((acc, hs) => {
+                acc.push({ hs: hs, ...getHsMetrics({ elevi: groupedStudents[hs] as Elev[], profesori: groupedTeachers[hs] as Profesor[], parinti: groupedParents[hs] as Parinte[] }) });
+                return acc;
+            }, []);
+
+            res.status(200).json(groupedResults);
+            return;
+        }
+
+        const results = [studentResults, teachersResults, parentResults].flat();
+
+        let highschools = [] as { name: string, records: number }[];
+        for (let i = 0; i < results.length; i++) {
+            if (highschools.length == 0) {
+                highschools.push({ name: results[i].hs, records: 1 });
+            } else {
+                let found = false;
+                for (let j = 0; j < highschools.length; j++) {
+                    if (highschools[j].name == results[i].hs) {
+                        highschools[j].records++;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    highschools.push({ name: results[i].hs, records: 1 });
                 }
             }
-            if (!found) {
-                highschools.push({ name: results[i].hs, records: 1 });
-            }
-        }
-    }
-
-    if (req.method == 'GET') {
-        let metrics = [] as { highschool: string, elevi: number, profesori: number, parinti: number }[];
-        for (let i = 0; i < highschools.length; i++) {
-            const studentResults = await prisma.elev.findMany({
-                where: {
-                    hs: highschools[i].name,
-                },
-            });
-
-            const teacherResults = await prisma.profesor.findMany({
-                where: {
-                    hs: highschools[i].name,
-                },
-            });
-
-            const parentResults = await prisma.parinte.findMany({
-                where: {
-                    hs: highschools[i].name,
-                },
-            });
-
-
-            const hsMetrics = getHsMetrics({ elevi: studentResults, profesori: teacherResults, parinti: parentResults }); //.scores.real
-
-            metrics.push({ highschool: highschools[i].name, ...hsMetrics });
         }
 
-        metrics.sort((a, b) => b.scores.real - a.scores.real);
-        res.status(200).json(metrics);
-        return;
-    }
-    else if (req.method == 'POST') {
         res.status(200).json(highschools);
         return;
     }
+
+    res.status(405).json({ message: 'Method not allowed' });
+    return;
 }
